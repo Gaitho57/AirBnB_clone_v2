239969e1e8b9676d46918c8caf91b92cc3066602
Celestine kibor <122092941+Celeekeybor@users.noreply.github.com>
2023-09-23 16:14:46 -0700

    state initialization

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
+
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" class State"""
+from os import getenv
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import sqlalchemy
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """Represent state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initialization of the state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """city instances related to the state using getter"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list

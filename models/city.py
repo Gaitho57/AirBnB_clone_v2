1e00735976e6383c9708d8dd1fd5aea5475d6077
Celestine kibor <122092941+Celeekeybor@users.noreply.github.com>
2023-09-23 16:10:30 -0700

    hold the city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,24 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class City"""
+import models
+from os import getenv
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """Represent city"""
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """city initialization"""
+        super().__init__(*args, **kwargs)

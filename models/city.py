134455f8801a645c112132ad2f06966ac067476a
Gaitho57 <122827143+Gaitho57@users.noreply.github.com>
2023-09-24 04:10:13 +0300

    Update_city

--- models/city.py
+++ models/city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""City class."""
+"""Defines the City class."""
 from models.base_model import Base
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
 from models.base_model import BaseModel
 from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
@@ -12,6 +12,11 @@ class City(BaseModel, Base):
     """Represents a city for a MySQL database.
 
     Inherits from SQLAlchemy Base and links to the MySQL table cities.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)

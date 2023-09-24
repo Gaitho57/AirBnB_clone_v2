1515de95f5bd29a97b3658a91f0341ba0a4d1146
Gaitho57 <122827143+Gaitho57@users.noreply.github.com>
2023-09-24 04:08:11 +0300

    Update_review

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Review class."""
+"""Defines the Review class."""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -9,8 +9,15 @@ from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """
+    """Represents a review for a MySQL database.
+
     Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
     """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
